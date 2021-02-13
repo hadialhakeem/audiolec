20931037473640ff7a17f0d6f6d3f33d58dde575
Yasith Ellewela <yasith.ellewela02@gmail.com>
2021-02-13 17:32:45 -0500

    Added steps to landing page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Button } from 'antd';
 import './App.css';
 import Main from "./components/Main";
 import Header from "./components/Title";
+import Steps from "./components/Steps";
 
 const App = () => (
     <div className="App">
         <Header />
         <Main />
         <Button type="primary">Button</Button>
+        <Steps />
         
     </div>
 );
--- src/components/Steps.js
+++ src/components/Steps.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { Card } from 'antd';
+import { Row, Col } from 'antd';
+
 
 class Steps extends React.Component {
     constructor(props) {
@@ -8,12 +11,29 @@ class Steps extends React.Component {
 
     render() {
         return (
-            <div>
-                Main
+            <div className="site-card-border-less-wrapper">
+            <Row>
+                <Col xs={{ span: 5, offset: 1 }} lg={{ span: 6, offset: 2 }}>
+                    <Card headStyle ={{color: "#FFFFFF", fontSize: 30}} title="Step 1"  bordered={false} style={{ width: 300, height: 200, color: "#FFFFFF", backgroundColor: "#B6A6CA", fontSize: 20, font: "Segoe UI"  }}>
+                        <p>Upload audio file (MP3 or WAV)</p>
+                    </Card>
+                </Col>
+                <Col xs={{ span: 11, offset: 1 }} lg={{ span: 6, offset: 2 }}>
+                    <Card headStyle ={{color: "#FFFFFF", fontSize: 30}} title="Step 2"  bordered={false} style={{ width: 300, height: 200, color: "#FFFFFF", backgroundColor: "#B6A6CA", fontSize: 20, font: "Segoe UI"  }}>
+                        <p>Wait for AudioLec to transcribe your file.</p>
+                    </Card>
+                </Col>
+                <Col xs={{ span: 5, offset: 1 }} lg={{ span: 6, offset: 2 }}>
+                    <Card headStyle ={{color: "#FFFFFF", fontSize: 30}} title="Step 3"  bordered={false} style={{ width: 300, height: 200, color: "#FFFFFF", backgroundColor: "#B6A6CA", fontSize: 20, font: "Segoe UI"  }}>
+                        <p>Download the transcript!</p>
+                    </Card>
+                </Col>
+            </Row>
             </div>
         )
     }
 }
+
 
 export default Steps;
 
