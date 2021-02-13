dcee3b98dbf26c12f3740bc30d1a8e2d6967647c
hadialhakeem <hadihakeem2@gmail.com>
2021-02-13 12:42:49 -0500

    changed stuff

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-function App() {
-  return (
-    <div>
+import React from 'react';
+import { Button } from 'antd';
+import './App.css';
+
+const App = () => (
+    <div className="App">
+        <Button type="primary">Button</Button>
     </div>
-  );
-}
+);
 
 export default App;
