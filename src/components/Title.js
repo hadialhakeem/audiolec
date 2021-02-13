09e0ca5b0cd6cc2d60b8352cd8e7f43f91b4a77d
hadialhakeem <hadihakeem2@gmail.com>
2021-02-13 13:06:54 -0500

    added boilerplate

---
+++ src/components/Title.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Title extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div>
+                Title
+            </div>
+        )
+    }
+}
+
+export default Title;
