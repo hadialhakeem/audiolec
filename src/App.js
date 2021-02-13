21133346201c73b3cb488f2cb021dd00568a334e
hadialhakeem <hadihakeem2@gmail.com>
2021-02-13 13:18:10 -0500

    imported main

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Button } from 'antd';
 import './App.css';
+import Main from "./components/Main";
 
 const App = () => (
     <div className="App">
         <Button type="primary">Button</Button>
+        <Main />
     </div>
 );
 
