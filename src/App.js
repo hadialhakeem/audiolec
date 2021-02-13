aae3ec8911cdfaadbd4c2ef2f71d9565d6174a0e
hadialhakeem <hadihakeem2@gmail.com>
2021-02-13 13:20:29 -0500

    imported title

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Button } from 'antd';
 import './App.css';
 import Main from "./components/Main";
+import Title from "./components/Title";
 
 const App = () => (
     <div className="App">
         <Button type="primary">Button</Button>
+        <Title />
         <Main />
     </div>
 );
