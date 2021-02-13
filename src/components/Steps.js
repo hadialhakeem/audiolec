301139601ae03dfdbdd9a76f78532eda66453bcf
Yasith Ellewela <yasith.ellewela02@gmail.com>
2021-02-13 15:40:29 -0500

    added steps component

---
+++ src/components/Steps.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Steps extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div>
+                Main
+            </div>
+        )
+    }
+}
+
+export default Steps;
+
