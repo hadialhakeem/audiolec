be9cec297b192b1a473041531c0a8a2d12b45efa
ElhamBadri2411 <elhambadri24@gmail.com>
2021-02-13 21:17:42 -0500

    string to text

--- src/apis/transcript_to_pdf.js
+++ src/apis/transcript_to_pdf.js
@@ -1,5 +1,12 @@
-function stringToBase64(transcription){
-    let transcriptionBase64 = Buffer.from(transcription).toString('base64');
-    console.log(transcriptionBase64)
-    return transcriptionBase64;
-}
+function downloadString(text) {
+    var blob = new Blob([text], { type: fileType });
+    var a = document.createElement('a');
+    a.download = "audiotranscript.txt";
+    a.href = URL.createObjectURL(blob);
+    a.dataset.downloadurl = ["text", a.download, a.href].join(':');
+    a.style.display = "none";
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    setTimeout(function() { URL.revokeObjectURL(a.href); }, 1500);
+};
